129cc14fbabe113e3fdabee11d27e82c227e9685
ada-mb <a.d.matveeva@yandex.ru>
2023-10-14 22:44:53 +0700

    чекстайл и логика программы перестали драться

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -7,26 +7,28 @@ public class Task1 {
     }
 
     public static long minutesToSeconds(String str) {
-        long result;
+        long result = 0;
         final int secondsInMinute = 60; //теперь это неизменяемая константа
         try {
             Objects.requireNonNull(str); //выбросит исключение NullPointerException если str == null
             int strLen = str.length();
             int lastIndexOfColon = str.lastIndexOf(":"); //вернет -1 если не нашел ":"
             if (lastIndexOfColon == -1) {
-                result = -1;
+                return -1;
             }
             int seconds = Integer.parseInt(str.substring(lastIndexOfColon + 1, strLen)); /*substring does
             mm:ss -> ss */
             /*Integer.parseInt(x) выбросит нам исключение NumberFormatException, если х не преобразовывается
             в число  */
             if (seconds >= secondsInMinute) {
-                result = -1;
+                return -1;
             }
 
             int minutes = Integer.parseInt(str.substring(0, lastIndexOfColon)); //substring does mm:ss -> mm
 
-            result = minutes * secondsInMinute + seconds;
+            if (result != -1) {
+                result = minutes * secondsInMinute + seconds;
+            }
         } catch (NullPointerException npe) {
             result = -1;
         } catch (NumberFormatException nfe) {
