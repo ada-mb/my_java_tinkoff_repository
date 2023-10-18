7d8a76b5d4da1d621ddacc2b4f807e04057095aa
ada-mb <a.d.matveeva@yandex.ru>
2023-10-19 01:07:20 +0700

    сделала задание 2 дз 2 тесты + чекстайл

--- src/main/java/edu/hw2/Task2/Rectangle.java
+++ src/main/java/edu/hw2/Task2/Rectangle.java
@@ -1,10 +1,13 @@
 package edu.hw2.Task2;
 
 public class Rectangle {
-    public final int width;
-    public final int height;
+    private final int width;
+    private final int height;
 
     public Rectangle(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException();
+        }
         this.width = width;
         this.height = height;
     }
